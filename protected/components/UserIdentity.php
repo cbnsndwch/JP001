748c43f5a1b07c5992b6d3e1dc837294815b0ebd
mark-c-valderrama <mark.c.valderrama@gmail.com>
2014-03-23 13:34:50 -0700

    Revert "bad commit"
    This reverts commit 678427aa453cea1cc41d558eee9feead4c94a52a.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -1,16 +1,14 @@
 <?php
 
 /**
- * InstructorIdentity represents the data needed to identity a user.
+ * UserIdentity represents the data needed to identity a user.
  * It contains the authentication method that checks if the provided
  * data can identity the user.
  */
-class InstructorIdentity extends CUserIdentity
+class UserIdentity extends CUserIdentity
 {
-        private $_id;
-
 	/**
-	 * Authenticates an instructor.
+	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
 	 * are both 'demo'.
 	 * In practical applications, this should be changed to authenticate
@@ -19,25 +17,17 @@ class InstructorIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$instructor=Instructor::model()->find('LOWER(username)=?',array(strtolower($this->username)));
-		if($user===null)
+		$users=array(
+			// username => password
+			'demo'=>'demo',
+			'admin'=>'admin',
+		);
+		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if(!$user->validatePassword($this->password))
+		elseif($users[$this->username]!==$this->password)
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
 		else
-		{
-			$this->_id=$user->id;
-			$this->username=$instructor->username;
 			$this->errorCode=self::ERROR_NONE;
-		}
-		return $this->errorCode==self::ERROR_NONE;
-	}
-
-	/**
-	 * @return integer the ID of the user record
-	 */
-	public function getId()
-	{
-		return $this->_id;
+		return !$this->errorCode;
 	}
 }
